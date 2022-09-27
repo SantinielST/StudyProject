f03cb8fc363fe3c95f05f17bc996d374cc52375b
Черных Павел <santiniel@gmail.com>
2022-09-27 15:34:16 +0300

    Base class Command

--- StudyProject/ViewModels/Command.cs
+++ StudyProject/ViewModels/Command.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace StudyProject.ViewModels
 {
-    public class Command
+    internal abstract class Command : ICommand
     {
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
 
+        public abstract bool CanExecute(object? parameter);
+
+        public abstract void Execute(object? parameter);
     }
 }
