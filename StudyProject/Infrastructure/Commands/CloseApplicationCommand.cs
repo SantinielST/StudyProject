446b0a831c31345d2b35990a8c96fc90f61a1fc0
Черных Павел <santiniel@gmail.com>
2022-09-28 11:14:41 +0300

    Command MainWindow

---
+++ StudyProject/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,12 @@
+﻿using StudyProject.Infrastructure.Commands.Base;
+using System.Windows;
+
+namespace StudyProject.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+    }
+}
