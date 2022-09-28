87500f60006ee4d41d17cfff0abdd7fcf34c8b1c
Черных Павел <santiniel@gmail.com>
2022-09-28 13:01:53 +0300

    Response to server for get raw data

--- Tests/StudyProjectConsole/Program.cs
+++ Tests/StudyProjectConsole/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Net;
+
+namespace StudyProjectConsole 
+{
+    class Program
+    {
+        private const string dataUrl = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+        
+        static void Main(string[] args)
+        {
+            //WebClient webClient = new WebClient();
+            var client = new HttpClient();
+
+            var response = client.GetAsync(dataUrl).Result;
+            var csvStr = response.Content.ReadAsStringAsync().Result;
+
+            Console.ReadLine();        }
+    }
+}
